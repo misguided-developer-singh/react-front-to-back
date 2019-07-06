0d09261633f6b1a89a8792397005ad7d0e81f218
misguided-developer-singh <amarjitkumarsingh25@gmail.com>
2019-07-06 16:43:00 +0530

    Updates

---
+++ precache-manifest.aaad2484b861fd47607e1f7e5450cdde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248c5fa50af2d91c53132833d746ab49",
+    "url": "/misguided-developer-singh/react-front-to-back/index.html"
+  },
+  {
+    "revision": "99ac18ccf7bd8f70ae02",
+    "url": "/misguided-developer-singh/react-front-to-back/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5001ef2360dd02a6d60c",
+    "url": "/misguided-developer-singh/react-front-to-back/static/css/main.0462ddb6.chunk.css"
+  },
+  {
+    "revision": "99ac18ccf7bd8f70ae02",
+    "url": "/misguided-developer-singh/react-front-to-back/static/js/2.861da97a.chunk.js"
+  },
+  {
+    "revision": "5001ef2360dd02a6d60c",
+    "url": "/misguided-developer-singh/react-front-to-back/static/js/main.b68c4b3b.chunk.js"
+  },
+  {
+    "revision": "bf82ee4cb78067dea09f",
+    "url": "/misguided-developer-singh/react-front-to-back/static/js/runtime~main.b70b013f.js"
+  }
+]);
